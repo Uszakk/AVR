854d472c441935f26a8b31e6c86f676a08fae2bb
Uszakk <149823354+Uszakk@users.noreply.github.com>
2024-05-25 14:53:26 +0200

    Update USART.h

--- USART.h
+++ USART.h
@@ -1,8 +1,16 @@
-#define F_CPU 8000000UL
-#define BAUD 9600
+//Basic functions for operating USART transmition
 #include <avr/io.h>
 #include <util/setbaud.h>
 
+//Default baudrate
+#define BAUD 9600
+
+//Initializaton of USART comunication
 void USART_init(void);
+
+//Function for reciving one frame (setted in initialization) of data by USART
+//Returns one byte of data from UDR register
 uint8_t USART_receive(void);
+
+//Function for transmiting one frame (setted in initialization) of data by USART
 void USART_transmit(uint8_t data);
