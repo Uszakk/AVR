f51fb44060f91dabcc2a5df504b55e43a05f4d43
Uszakk <149823354+Uszakk@users.noreply.github.com>
2024-10-20 12:02:38 +0200

    Update Encoder.c

--- Encoder.c
+++ Encoder.c
@@ -1,16 +1,7 @@
 //Functions and macros for setup and operating rotary encoder
 #include <avr/io.h>
 #include <util/delay.h>
-
-//Time in miliseconds that must past to read the encoder state again (it should be not longer than 255 ms because it is stored as uint8_t type)
-#define IGNORE_TIME 5
-
-//Default connection of encoder
-#define ENCODER_A PB0
-#define ENCODER_B PB1
-#define ENCODER_DDR DDRB
-#define ENCODER_PORT PORTB
-#define ENCODER_PIN PINB
+#include "Encoder.h"
 
 //Initializtion of encoder (should be placed later in code for prevent overwrite)
 void Encoder_init(void){
